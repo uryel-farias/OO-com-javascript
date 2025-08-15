db4d0d940f74d7252f451eca18d516e64eacf2e6
Uryel Farias <uryel.farias@gmail.com>
2025-08-15 11:01:22 -0300

    Exibindo uma informação do objeto e entendendo sobre this e bind.

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -4,8 +4,15 @@ const user = {
     nascimento: '2024-01-01',
     role: 'estudante',
     ativo: true,
-    
+
     exibirInfos: function() {
         console.log(this.nome, this.email)
     },
 }
+
+const exibir = function() {
+    console.log(this.nome, this.email);
+}
+
+const exibirNome = exibir.bind(user);
+exibirNome()
