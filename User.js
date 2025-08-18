38f9c7d5a5ef7dcd876218d51724b07c9335c92f
Uryel Farias <uryel.farias@gmail.com>
2025-08-18 11:33:18 -0300

    Criando uma classe com im construtor

---
+++ User.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(nome, email, nascimento, role, ativo = true) {
+        this.nome = nome;
+        this.email = email;
+        this.nascimento = nascimento;
+        this.role = role || "estudante";
+        this.ativo = ativo;
+    }
+
+    exibirInfos() {
+        return `${this.nome}, ${this.email}`
+    }
+}
+
+const novoUser = new User('Uryel', 'uryel@email', '2025-11-25');
+
+console.log(novoUser);
+console.log(novoUser.exibirInfos());
