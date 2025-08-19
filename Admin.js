3c664e3011a4d1b59fbb6e5ca914067c5fe42f54
Uryel Farias <uryel.farias@gmail.com>
2025-08-18 23:49:08 -0300

    Adicionamdo o metodo criarCurso em novoAdmin

--- Admin.js
+++ Admin.js
@@ -4,8 +4,12 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = "Admin", ativo = true) {
         super(nome, email, nascimento, role, ativo)
     }
+
+    criarCurso(nomeCurso, qtdVagas){
+        return `Curso ${nomeCurso} criado com ${qtdVagas} vagas.`
+    }
 }
 
 const novoAdmin = new Admin("Livia", "livia@email", "15-11-2022");
 console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('Js', 20));
