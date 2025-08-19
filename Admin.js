a51a90577b498004771e9dd05e548d2a58cdde6e
Uryel Farias <uryel.farias@gmail.com>
2025-08-18 23:25:30 -0300

    Criando uma nova classe

---
+++ Admin.js
@@ -0,0 +1,11 @@
+import User from "./User.js";
+
+class Admin extends User {
+    constructor(nome, email, nascimento, role = "Admin", ativo = true) {
+        super(nome, email, nascimento, role, ativo)
+    }
+}
+
+const novoAdmin = new Admin("Livia", "livia@email", 15-11-22);
+console.log(novoAdmin);
+console.log(novoAdmin.exibirInfos());
